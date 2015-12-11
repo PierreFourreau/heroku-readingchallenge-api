08c5400033f6845b35a402b627b0fbe8491b41e3
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-11 21:28:59 +0100

    update

--- web/index.php
+++ web/index.php
@@ -43,5 +43,140 @@ function getCategories() {
 	}
 }
 
+//category
+$app->get('/categories', function ($request, $response, $args) {
+  $sql = "select c.id, c.libelle_fr, c.libelle_en, c.description_fr, c.description_en, c.image FROM categories c";
+  try {
+    $db = getConnection();
+    $stmt = $db->query($sql);
+    $categories = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($categories);
+    exit;
+  } catch(Exception $e) {
+    $app = \Slim\Slim::getInstance();
+    $app->log->error('getCategories-'.$e->getMessage());
+    echo json_encode($categories);
+    exit;
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+$app->get('/categoriesByLevel/{level}', function ($request, $response, $args) {
+  $sql = "SELECT c.id, c.libelle_fr, c.libelle_en, c.description_fr, c.description_en, c.image FROM categories c where c.niveau<=:level";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("level", $args['level']);
+    $stmt->execute();
+    $categories = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($categories);
+    exit;
+  } catch(Exception $e) {
+    $app = \Slim\Slim::getInstance();
+    $app->log->error('getCategoriesByLevel-'.$e->getMessage());
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+});
+
+$app->get('/categories/{id}', function ($request, $response, $args) {
+  $sql = "SELECT c.id, c.libelle_fr, c.libelle_en, c.description_fr, c.description_en, c.image FROM categories c WHERE c.id=:id";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("id", $args['id']);
+    $stmt->execute();
+    $categorie = $stmt->fetchObject();
+    $db = null;
+    echo json_encode($categorie);
+    exit;
+  } catch(Exception $e) {
+    $app = \Slim\Slim::getInstance();
+    $app->log->error('getCategorie-'.$e->getMessage());
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+});
+
+$app->post('/categories', function ($request, $response, $args) {
+  //$request = Slim::getInstance()->request();
+  $categorie = json_decode($request->getBody());
+  $sql = "INSERT INTO categories(libelle_en, libelle_fr, description_en, description_fr, image) VALUES (:libelle_en, :libelle_fr, :description_en, :description_fr, :image)";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("libelle_en", $categorie->libelle_en);
+    $stmt->bindParam("libelle_fr", $categorie->libelle_fr);
+    $stmt->bindParam("description_en", $categorie->description_en);
+    $stmt->bindParam("description_fr", $categorie->description_fr);
+    $stmt->bindParam("image", $categorie->image);
+    $stmt->execute();
+    $categorie->id = $db->lastInsertId();
+    $db = null;
+    echo json_encode($categorie);
+    exit;
+  } catch(Exception $e) {
+    $app = \Slim\Slim::getInstance();
+    $app->log->error('addCategorie-'.$e->getMessage());
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+$app->get('/suggestionsByCategory/{id}', function ($request, $response, $args) {
+  $sql = "SELECT s.id, s.libelle_fr, s.libelle_en, s.categorie_id FROM suggestions s WHERE s.id=:id";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("id", $args['id']);
+    $stmt->execute();
+    $suggestion = $stmt->fetchObject();
+    $db = null;
+    echo json_encode($suggestion);
+    exit;
+  } catch(Exception $e) {
+    $app = \Slim\Slim::getInstance();
+    $app->log->error('getSuggestionById-'.$e->getMessage());
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+/*$app->post('/propositions', function ($request, $response, $args) {
+  //$request = \Slim\Slim::getInstance()->request();
+  $proposition = json_decode($request->getBody());
+  $sql = "INSERT INTO propositions(libelle_en, libelle_fr, categorie_id, created, modified) VALUES (:libelle_en, :libelle_fr, :id, :dateNow, :dateNow)";
+  parse_str($request->getBody(), $params);
+  $dateNow = date("Y-m-d H:i:s");
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("libelle_en", $params['libelle_en']);
+    $stmt->bindParam("libelle_fr", $params['libelle_fr']);
+    $stmt->bindParam("id", $params['categorie_id']);
+    $stmt->bindParam("dateNow", $dateNow);
+    $stmt->execute();
+    $id = $db->lastInsertId();
+    $db = null;
+    echo json_encode($id);
+    //send email
+
+    $headers = "From: ReadingChallenge\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $email = 'readingchallenge.contact@gmail.com';
+    $subject = 'Readingchallenge - ajout proposition';
+    $message = '<html><body>';
+    $message .= 'Nouvelle proposition ajoutée<br/><br/>';
+    $message .= 'Libelle fr : ' . $params['libelle_fr'].'<br/>';
+    $message .= 'Libelle en : ' . $params['libelle_en'];
+    $message .= '<br/><br/><a href="http://pierrefourreau.fr/readingchallenge/readingchallenge-admin/propositions">Admin</a>';
+    $message .= '</body></html>';
+    mail($email, $subject, $message, $headers);
+    exit;
+  } catch(Exception $e) {
+    $app = \Slim\Slim::getInstance();
+    $app->log->error('addProposition-'.$e->getMessage());
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}*/
 
 $app->run();
