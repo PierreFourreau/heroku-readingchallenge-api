3589663f07738d51f037550eac5063df6ab8b99a
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-11 18:29:32 +0100

    Testing database connexion

--- web/index.php
+++ web/index.php
@@ -3,7 +3,33 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require('../vendor/autoload.php');
-require 'db.php';
+//require 'db.php';
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+echo $url;
+exit();
+const DB_SERVER = $url["host"];
+const DB_USER = $url["user"];
+const DB_PASSWORD = $url["pass"];
+const DB = substr($url["path"], 1);
+
+function getConnection() {
+	$dbhost=DB_SERVER;
+	$dbuser=DB_USER;
+	$dbpass=DB_PASSWORD;
+	$dbname=DB;
+
+echo $dbhost;
+echo $dbuser;
+echo $dbpass;
+echo $dbname;
+
+	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+	//$dbh->exec("set names utf8");
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbh;
+}
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
