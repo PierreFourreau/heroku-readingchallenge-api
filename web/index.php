29bc6243510c8bbf5a1daea1395083cf8a3162e3
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-11 18:42:09 +0100

    Testing database connexion

--- web/index.php
+++ web/index.php
@@ -8,13 +8,14 @@ require('../vendor/autoload.php');
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-
+print_r($url);
 const DB_SERVER = $url["host"];
 const DB_USER = $url["user"];
 const DB_PASSWORD = $url["pass"];
-const DB = substr($url["path"], 1);
-
 print_r($url);
+//const DB = substr($url["path"], 1);
+
+
 
 
 
