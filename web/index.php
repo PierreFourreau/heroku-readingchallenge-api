1611ec8768a9da4a0e655224cb81e857b1933d22
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-12 15:30:20 +0100

    update

--- web/index.php
+++ web/index.php
@@ -187,9 +187,9 @@ $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
-$mail->Port = 465; // or 587
+$mail->Port = 587; // or 587
 $mail->IsHTML(true);
 $mail->Username = "readingchallenge.contact@gmail.com";
 $mail->Password = "76hdfrb8";
