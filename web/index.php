09607c0180d10e4787b9cd5627e3b81dcd12b454
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-12 15:28:15 +0100

    update

--- web/index.php
+++ web/index.php
@@ -187,7 +187,7 @@ $mail = new PHPMailer(); // create a new object
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
