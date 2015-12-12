d93ba78f201e5f2d758de6d15bdad03aad167ecf
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-12 18:14:40 +0100

    update

--- web/index.php
+++ web/index.php
@@ -144,6 +144,18 @@ file_put_contents("php://stderr", "error suggestionsByCategory : " . $e->getMess
 });
 
 $app->post('/propositions', function ($request, $response, $args) {
+  $mgClient = new Mailgun('key-2c6b1db7c0f95a7be2ea5386e7838281');
+  $domain = "sandbox2c6c61e0b2ae480e8a93264230550a57.mailgun.org";
+
+  # Make the call to the client.
+  $result = $mgClient->sendMessage("$domain",
+                    array('from'    => 'ReadingChallenge <readingchallenge.contact@gmail.com>',
+                          'to'      => 'Pierre <readingchallenge.contact@gmail.com>',
+                          'subject' => 'Hello Pierre',
+                          'text'    => 'Congratulations Pierre, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
+
+
+
 
 
 
@@ -165,17 +177,6 @@ $app->post('/propositions', function ($request, $response, $args) {
     $id = $db->lastInsertId();
     $db = null;
     echo json_encode($id);
-
-    $mgClient = new Mailgun('key-2c6b1db7c0f95a7be2ea5386e7838281');
-    $domain = "sandbox2c6c61e0b2ae480e8a93264230550a57.mailgun.org";
-
-    # Make the call to the client.
-    $result = $mgClient->sendMessage("$domain",
-                      array('from'    => 'ReadingChallenge <readingchallenge.contact@gmail.com>',
-                            'to'      => 'Pierre <readingchallenge.contact@gmail.com>',
-                            'subject' => 'Hello Pierre',
-                            'text'    => 'Congratulations Pierre, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
-
 
 
     exit;
