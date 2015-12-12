bfd2f91ec09111d39e4d3b9a0fa9689fe9cb4b69
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-12 15:34:06 +0100

    update

--- web/index.php
+++ web/index.php
@@ -184,7 +184,7 @@ $mail->AddAddress("readingchallenge.contact@gmail.com");
 $mail->Send();*/
 
 $mail = new PHPMailer(); // create a new object
-$mail->IsSMTP(); // enable SMTP
+//$mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
 $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
