c166b9215506765e79800f60f29290526940f49e
Pierre Fourreau <fourreau.pierre@gmail.com>
2015-12-12 15:21:26 +0100

    update

--- web/index.php
+++ web/index.php
@@ -164,7 +164,7 @@ $app->post('/propositions', function ($request, $response, $args) {
 $mail->IsSMTP(); // enable SMTP
 $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
 $mail->SMTPAuth = true; // authentication enabled
-$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->SMTPSecure = 'tls'; // secure transfer enabled REQUIRED for Gmail
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 465; // or 587
 $mail->IsHTML(true);
